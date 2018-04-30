01bf6d2f0d95fd92e9e92532da83d14f544ea115
heathcliff-hu <hrc530756164@gmail.com>
2018-04-30 18:47:31 +0800

    middleware test

--- example/middleware.js
+++ example/middleware.js
@@ -1,11 +1,11 @@
-let TinyKoa = require('../src/application');
+let TinyKoa = require("../src/application");
 
 let app = new TinyKoa();
 
 let responseData = {};
 
 app.use(async(ctx, next) => {
-  responseData.name = 'Heathcliff Huu';
+  responseData.name = "Heathcliff Huu";
   await next();
   ctx.body = responseData;
 });
@@ -16,10 +16,10 @@ app.use(async(ctx, next) => {
 });
 
 app.use(async(ctx, next) => {
-  responseData.sex = 'Man';
+  responseData.sex = "Man";
   await next();
 });
 
 app.listen(3000, () => {
-  console.log('Listenning on 3000');
+  console.log("Listening on 3000");
 });
